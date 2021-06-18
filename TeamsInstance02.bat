c6fae5caef4460f1a0dee1c7feaf55c66ad3f5ce
Gustavo RV 86 <gusatvorv86@github.com>
2021-06-18 16:59:18 +0200

    Add TeamsInstance02 batch script.

---
+++ TeamsInstance02.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: Uses the file name as the profile name.
+set MSTEAMSPROFILE=%~n0
+echo --- Using profile "%MSTEAMSPROFILE%"
+
+:: Change USERPROFILE environment variable.
+set "CURRENTUSERPROFILE=%USERPROFILE%"
+set "USERPROFILE=%CURRENTUSERPROFILE%\CustomProfiles\%MSTEAMSPROFILE%"
+
+:: Ensure there is a Downloads folder.
+mkdir "%USERPROFILE%\Downloads"
+
+:: Start the new instance of Teams.
+echo --- Launching MS Teams with profile %MSTEAMSPROFILE%
+cd "%CURRENTUSERPROFILE%\AppData\Local\Microsoft\Teams"
+"%CURRENTUSERPROFILE%\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe"
+
